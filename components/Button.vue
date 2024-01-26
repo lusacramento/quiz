ba449f7694390b6d0ca95c040b40779d27cc99ef
lusacramento <lucianossacramento@yahoo.com.br>
2024-01-26 12:55:11 -0300

    feat button template

---
+++ components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+	<button type="button" class="reset-btn">Reset</button>
+</template>
+
+<script lang="ts" setup></script>
+
+<style>
+	.reset-btn {
+		background-color: #ff6372;
+		border: 0;
+		font-size: 22px;
+		color: #fff;
+		padding: 10px 25px;
+		margin: 10px auto;
+		display: block;
+	}
+
+	.reset-btn:active,
+	.reset-btn:focus,
+	.reset-btn:hover {
+		border: 0;
+		outline: 0;
+	}
+
+	.reset-btn:hover {
+		background-color: #ee4d5a;
+	}
+</style>
