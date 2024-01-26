38a9d2d25b7b7ea37da799393470bf2689ddd4ca
lusacramento <lucianossacramento@yahoo.com.br>
2024-01-26 12:56:05 -0300

    feat page template

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<Questions />
+</template>
+
+<script lang="ts" setup></script>
+
+<style>
+	* {
+		box-sizing: border-box;
+	}
+	body {
+		font-size: 20px;
+		font-family: sans-serif;
+		padding-top: 20px;
+		background: #e6ecf1;
+	}
+</style>
